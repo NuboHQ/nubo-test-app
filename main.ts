9cfc260cb3f3519ffef6a2be6823af8d390068c1
Marcel Thomas <marcelthomas5@gmail.com>
2023-01-10 16:04:54 +0000

    Initial commit

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text(`Hi there from ${process.env.NUBO_REGION}!`);
+});
+
+export default {
+  port: process.env.PORT,
+  fetch: app.fetch,
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Title
+
+This is a test.
+
+```
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+```
+
+1. First item
+2. Second item
+3. Third item
+
+> An amazing quote from someone important!
